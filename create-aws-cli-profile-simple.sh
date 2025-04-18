a1de546bc5ce35f4cec9f6e5f8a04d9ff008b9a9
Teri Radichel <github@pwn.fyi>
2025-04-18 18:29:00 -0400

    Create create-aws-cli-profile-simple.sh

---
+++ create-aws-cli-profile-simple.sh
@@ -0,0 +1,19 @@
+echo "Enter profile name"
+read profile
+
+echo "Enter Key ID:"
+read aws_access_key_id
+
+echo "Enter secret access key:"
+read aws_secret_access_key
+
+echo "Enter region (e.g. us-east-1)"
+read region
+
+echo "Enter output format (e.g. json or text)"
+read output
+
+aws configure set aws_access_key_id $aws_access_key_id --profile $profile
+aws configure set aws_secret_access_key $aws_secret_access_key --profile $profile
+aws configure set region $region --profile $profile
+aws configure set output $output --profile $profile
